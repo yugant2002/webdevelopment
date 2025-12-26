38d27d06489f79092f8ebecc459cef03e9fffcf6
yugant2002 <yugantnath2002@gmail.com>
2025-12-26 18:03:34 +0530

    .

---
+++ REACT/Demo/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  let a = 5;
+  let b = 10;
+  console.log(a + b);
+
+  return (
+    <>
+      <h1 id="head">Hello World</h1>
+      <div>this is first React App</div>
+    </>
+  );
+}
+
+
+export default App;
