d26089d0bae829d1b2bdaab9b853a5c443fc11dc
yugant2002 <yugantnath2002@gmail.com>
2025-12-31 18:39:09 +0530

    doubt

--- REACT/Demo/src/pages/Contact.jsx
+++ REACT/Demo/src/pages/Contact.jsx
@@ -8,13 +8,28 @@ const Contact = () => {
     city: "",
     subject: "",
     message: "",
+    religion: "",
+    gender: "",
+    skill: [],
   });
 
   const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (e) => {
-    const { name, value } = e.traget;
-    setContactData((previousData) => ({ ...previousData, [name]: value }));
+    const { name, value, type, checked } = e.target;
+    if (type === "checkbox") {
+      let temp = contactData.skill;
+      if (checked) {
+        temp.push(value);
+        setContactData((previousData) => ({ ...previousData, [name]: temp }));
+      } else {
+        temp = Object.values(temp); //Convert to Array
+        temp = temp.filter((word) => word !== value); //Remove the Undersired Value
+        setContactData((previousData) => ({ ...previousData, [name]: temp }));
+      }
+    } else {
+      setContactData((previousData) => ({ ...previousData, [name]: value }));
+    }
   };
 
   const handleClearForm = () => {
@@ -25,6 +40,9 @@ const Contact = () => {
       city: "",
       subject: "",
       message: "",
+      religion: "",
+      gender: "",
+      skill: [],
     });
   };
 
@@ -47,7 +65,7 @@ const Contact = () => {
         <h1>Contact Us</h1>
         <div className="container">
           <form onReset={handleClearForm} onSubmit={handleSubmit}>
-            <div className="mb-2">
+            <div>
               <label htmlFor="fullName">Full Name</label>
               <input
                 type="text"
@@ -60,7 +78,7 @@ const Contact = () => {
               />
             </div>
 
-            <div className="mb-2">
+            <div>
               <label htmlFor="email">Email</label>
               <input
                 type="email"
@@ -73,7 +91,7 @@ const Contact = () => {
               />
             </div>
 
-            <div className="mb-2">
+            <div>
               <label htmlFor="phone">Phone</label>
               <input
                 type="number"
@@ -86,7 +104,7 @@ const Contact = () => {
               />
             </div>
 
-            <div className="mb-2">
+            <div>
               <label htmlFor="city">City</label>
               <input
                 type="text"
@@ -99,7 +117,7 @@ const Contact = () => {
               />
             </div>
 
-            <div className="mb-2">
+            <div>
               <label htmlFor="subject">Subject</label>
               <input
                 type="text"
@@ -112,7 +130,110 @@ const Contact = () => {
               />
             </div>
 
-            <div className="mb-2">
+            <div>
+              <label htmlFor="religion">Religion</label>
+              <select
+                name="religion"
+                id="religion"
+                onChange={handleChange}
+                value={contactData.religion}
+              >
+                <option value="">--Select Religion--</option>
+                <option value="islam">Islam</option>
+                <option value="hinduism">Hinduism</option>
+                <option value="christianity">Christianity</option>
+                <option value="buddhism">Buddhism</option>
+                <option value="jainism">Jainism</option>
+                <option value="sikhism">Sikhism</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+
+            <div>
+              <label htmlFor="gender">Gender</label>
+              <input
+                type="radio"
+                name="gender"
+                value="male"
+                onChange={handleChange}
+                checked={contactData.gender === "male"}
+              />{" "}
+              Male
+              <input
+                type="radio"
+                name="gender"
+                value="female"
+                onChange={handleChange}
+                checked={contactData.gender === "female"}
+              />{" "}
+              Female
+              <input
+                type="radio"
+                name="gender"
+                value="other"
+                onChange={handleChange}
+                checked={contactData.gender === "other"}
+              />{" "}
+              Other
+            </div>
+
+            <div>
+              <label htmlFor="skill">Skill known</label>
+              <input
+                type="checkbox"
+                name="skill"
+                value="html"
+                onChange={handleChange}
+                checked={
+                  Object.values(contactData.skill).find(
+                    (word) => word === "html"
+                  )
+                    ? true
+                    : false
+                }
+              />{" "}
+              HTML
+              <input
+                type="checkbox"
+                name="skill"
+                value="css"
+                onChange={handleChange}
+                checked={
+                  Object.values(contactData.skill).find(
+                    (word) => word === "css"
+                  )
+                    ? true
+                    : false
+                }
+              />{" "}
+              CSS
+              <input
+                type="checkbox"
+                name="skill"
+                value="js"
+                onChange={handleChange}
+                checked={
+                  Object.values(contactData.skill).find(
+                    (word) => word === "js"
+                  )
+                    ? true
+                    : false
+                }
+              />{" "}
+              JS
+              <input
+                type="checkbox"
+                name="skill"
+                value="react"
+                onChange={handleChange}
+                checked={
+                  Object.values(contactData.skill).includes("react")
+                }
+              />{" "}
+              React
+            </div>
+
+            <div>
               <label htmlFor="message">Message</label>
               <textarea
                 name="message"
@@ -123,11 +244,11 @@ const Contact = () => {
                 className="text-primary"
               ></textarea>
             </div>
-            <div className="d-flex justify-content-center gap-4">
-              <button type="reset" className="btn btn-danger ">
+            <div>
+              <button type="reset" className="btn btn-danger">
                 Clear Form
               </button>
-              <button type="submit" className="btn btn-success mr-5">
+              <button type="submit" className="btn btn-success">
                 {isLoading ? "Loading" : "Submit"}
               </button>
             </div>
